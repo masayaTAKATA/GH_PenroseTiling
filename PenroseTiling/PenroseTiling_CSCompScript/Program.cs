afe3ee81ac641ae02d63ded5f84e1cf15e816868
Masaya Takata <takata@meenaxy.com>
2020-04-08 09:14:19 +0900

    Update

--- PenroseTiling/PenroseTiling_CSCompScript/Program.cs
+++ PenroseTiling/PenroseTiling_CSCompScript/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Rhino;
+using Rhino.Geometry;
+
 
 namespace PenroseTiling_CSCompScript
 {
@@ -10,6 +13,9 @@ namespace PenroseTiling_CSCompScript
     {
         static void Main(string[] args)
         {
+            var pt = new Point3d(Point3d.Unset);
+            Console.WriteLine(pt.X);
+
         }
     }
 }
